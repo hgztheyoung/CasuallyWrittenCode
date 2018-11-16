ff84ae76d887311e32cba485de53c0a0b66cf09f
hgztheyoung <hgzdsb@gmail.com>
2018-11-16 16:06:43 +0800

    Update sum.go

--- sum.go
+++ sum.go
@@ -7,9 +7,12 @@ import (
 )
 
 func main() {
-    size := 10000
-    arr := rand.Perm(size)
-    c := make(chan int,10)
+    data_size := 100000
+    worker_count := 100
+    queue_size := 50
+    
+    arr := rand.Perm(data_size)
+    c := make(chan int,queue_size)
     go func(){
         for _,i :=range arr{
             c <- i
@@ -18,8 +21,8 @@ func main() {
     res := make(chan int,1)
     res <-0
     var wg sync.WaitGroup
-    wg.Add(size)
-    for count:=0;count<10;count++{
+    wg.Add(data_size)
+    for count:=0;count<worker_count;count++{
         go func(){ //Gopher means go func
             for {
                 i := <-c
