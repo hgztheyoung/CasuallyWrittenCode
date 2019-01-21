4d998e0d17151729ac625934cad58584dbac4bb8
hgztheyoung <hgzdsb@gmail.com>
2019-01-21 15:33:00 +0800

    Update go_parallel.go

--- go_parallel.go
+++ go_parallel.go
@@ -4,12 +4,101 @@ import (
 	"time"
 	"log"
 	"math/rand"
+	"fmt"
 )
 
 func Job(i int) {
 	<-time.After(time.Duration(1+rand.Intn(2)) * time.Second)
 	log.Println("Job ", i, " done")
 	return
+}
+
+func JobMayFail(i int) int {
+	<-time.After(time.Duration(10+rand.Intn(2)) * time.Microsecond)
+	flip := rand.Int31n(100)
+	if flip > 80 {
+		log.Println("Job ", i, " failed")
+		return -1
+	}
+	log.Println("Job ", i, " done")
+	return 0
+}
+
+func DoJobMayFailSequencial() {
+	jobCount := 500
+	undoneChan := make(chan int, jobCount)
+	doneChan := make(chan int, jobCount)
+	giveUpChan := make(chan int, jobCount)
+	failChan := make(chan int, jobCount)
+	failedCount := make(map[int]int)
+	for i := 0; i < jobCount; i++ {
+		undoneChan <- i
+	}
+	givenUpCount := 0
+
+	for len(doneChan)+len(giveUpChan) != jobCount {
+		select {
+		case i := <-undoneChan:
+			ret := JobMayFail(i)
+			if ret == 0 {
+				doneChan <- i
+			} else {
+				failChan <- i
+			}
+		case i := <-failChan:
+			failedCount[i]++
+			if failedCount[i] == 2 {
+				log.Println("Job ", i, " givenup")
+				giveUpChan <- i
+			} else {
+				undoneChan <- i
+			}
+		}
+	}
+	fmt.Println("len(undoneChan)", len(undoneChan))
+	fmt.Println("len(doneChan)", len(doneChan))
+	fmt.Println("len(giveUpChan)", len(giveUpChan), givenUpCount)
+	fmt.Println("len(failChan)", len(failChan))
+}
+func DoJobMayFailParallel() {
+	jobCount := 5000
+	undoneChan := make(chan int, jobCount)
+	doneChan := make(chan int, jobCount)
+	giveUpChan := make(chan int, jobCount)
+	failChan := make(chan int, jobCount)
+	failedCount := make(map[int]int)
+	for i := 0; i < jobCount; i++ {
+		undoneChan <- i
+	}
+	givenUpCount := 0
+
+	for len(doneChan)+len(giveUpChan) != jobCount {
+		select {
+		case i := <-undoneChan:
+			go func() {
+				ret := JobMayFail(i)
+				if ret == 0 {
+					doneChan <- i
+				} else {
+					failChan <- i
+				}
+			}()
+		case i := <-failChan:
+			failedCount[i]++
+			if failedCount[i] == 2 {
+				log.Println("Job ", i, " givenup")
+				giveUpChan <- i
+			} else {
+				undoneChan <- i
+			}
+		default:
+			continue
+		}
+	}
+	fmt.Println("len(undoneChan)", len(undoneChan))
+	fmt.Println("len(doneChan)", len(doneChan))
+	fmt.Println("len(giveUpChan)", len(giveUpChan), givenUpCount)
+	fmt.Println("len(failChan)", len(failChan))
 }
 
 func DoJobSequencial() {
@@ -45,4 +134,33 @@ func DoJobParallel() {
 		}()
 	}
 	<-allDone
+}
+
+func randomDispatch(from <-chan struct{}, outs []chan<- struct{}) {
+	for {
+		select {
+		case <-from:
+			outs[rand.Intn(len(outs))] <- struct{}{}
+		}
+	}
+}
+
+func PingPongGoroutine() {
+	c := make(chan struct{})
+	counter := 0
+	go func() {
+		for {
+			c <- struct{}{}
+			<-c
+			counter++
+		}
+	}()
+	go func() {
+		for {
+			<-c
+			c <- struct{}{}
+		}
+	}()
+	<-time.After(1 * time.Second)
+	fmt.Println(counter)
 }
