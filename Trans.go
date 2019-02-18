ebf1ec625654b56409bc59f65f2e84c6995cc466
hgztheyoung <hgzdsb@gmail.com>
2019-02-18 15:56:31 +0800

    Update Trans.go

--- Trans.go
+++ Trans.go
@@ -30,6 +30,16 @@ type Order struct {
 var Orders chan Order
 
 func OrderBinarySearch(array []Order, first, last int, value Order) int {
+	//deal with Market Order
+	if value.MarketLimit == Market {
+		if value.AskBid == Ask {
+			return 0
+		}
+		if value.AskBid == Bid {
+			return last
+		}
+	}
+
 	for first < last {
 		mid := first + (last-first)/2
 		if array[mid].Price < value.Price {
@@ -64,7 +74,11 @@ func BrokerMainLoop() {
 		}
 		//trade off the Asks and Bids
 		fmt.Println(len(Asks), len(Bids), len(Orders))
-		for len(Asks) > 0 && len(Bids) > 0 && Asks[0].Price <= Bids[len(Bids)-1].Price {
+
+		for len(Asks) > 0 && len(Bids) > 0 &&
+			(Asks[0].Price <= Bids[len(Bids)-1].Price ||
+				Asks[0].MarketLimit == Market ||
+				Bids[len(Bids)-1].MarketLimit == Market) {
 			if Asks[0].Amount < Bids[len(Bids)-1].Amount {
 				Bids[len(Bids)-1].Amount = Bids[len(Bids)-1].Amount - Asks[0].Amount
 				fmt.Println("Deal!!!", Asks[0], " -> ", Bids[len(Bids)-1])
@@ -84,7 +98,6 @@ func BrokerMainLoop() {
 				continue
 			}
 		}
-
 		if len(Asks) > 5 && len(Bids) > 5 {
 			fmt.Println("Loweast 5 Asks", Asks[0:5])
 			fmt.Println("Highest 5 Bids", Bids[len(Bids)-5:])
@@ -98,7 +111,11 @@ func DealerMainLoop() {
 		select {
 		case <-time.After(time.Millisecond * 15):
 			//create dummy orders and send to order chan
-			Orders <- Order{AskBid: AskBid(rand.Intn(2)), MarketLimit: Market, Amount: rand.Intn(1000), Price: float64(rand.Intn(1000))}
+			order := Order{AskBid: AskBid(rand.Intn(2)), MarketLimit: MarketLimit(rand.Intn(2)), Amount: rand.Intn(1000), Price: float64(rand.Intn(1000))}
+			if order.MarketLimit == Market {
+				order.Price = 0
+			}
+			Orders <- order
 		}
 	}
 }
@@ -107,7 +124,7 @@ func TestPlayWithChan(t *testing.T) {
 	//Init Orders
 	Orders = make(chan Order, 100000)
 	go BrokerMainLoop()
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 1000; i++ {
 		go DealerMainLoop()
 	}
 	select {}
