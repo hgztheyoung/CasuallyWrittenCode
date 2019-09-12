28611d6de3d6077df50287b7cab0bcb549957ee7
hgztheyoung <hgzdsb@gmail.com>
2019-09-12 16:55:40 +0800

    Update cps.rkt
    clean up a little bit

--- cps.rkt
+++ cps.rkt
@@ -1,14 +1,17 @@
 #lang racket
-;seemingly right cps convert,with some referring to yinwang's cps.ss
+
 #;(x -> (k x))
 #;(((m n) -> (m n k)))
 #;((m n) -> (m n (lambda (mnv) (k mnv))))
 #;((x (y z)) -> (y z (lambda (yzres) (x yzres k))))
-#;(((lambda (x) (m x)) (lambda (a) b)) -> ((lambda (x k0) (m x k0)) (lambda (a k1) (k1 b)) k))
+#;(((lambda (x) (m x)) (lambda (a) b)) -> ((lambda (x k0) (m x k0)) (k0 (lambda (a k1) (k1 b))) k))
+
+(define (atom-call? call)
+  (member call `(add1 sub1)))
 
 (define (atom? expr)
   (match expr
-    [x #:when (symbol? x) #t]
+    [x #:when (or (number? x) (symbol? x)) #t]
     [`(lambda (,x k) ,body) #t]
     [default #f]))
 
@@ -18,23 +21,24 @@
            [cps1 (lambda (expr codegen)
                    (match expr
                      [atom #:when (atom? atom) (codegen expr)]
-                     [`(,app ,rator) #:when (and (atom? app) (atom? rator))
+                     [`(,app ,rand) #:when (and (atom? app) (atom? rand))
                                      (if (eq? codegen id-gen)
-                                         `(,app ,rator k) 
+                                         `(,app ,rand k) 
                                          (let ([x (gensym "var")])
-                                           `(,app ,rator (lambda (,x) ,(codegen x)))))]
-                     [`(,app ,rator) (cps1 rator (lambda (r-atom)
+                                           `(,app ,rand (lambda (,x) ,(codegen x)))))]
+                     [`(,app ,rand) (cps1 rand (lambda (r-atom)
                                                    (cps1 app (lambda (a-atom)
                                                                (cps1 `(,a-atom ,r-atom) codegen)))))]
-                     [`(lambda (,x) ,body) (codegen `(lambda (,x k) ,(cps1 body id-gen)))]))])
+                     [`(lambda (,x) ,body) (codegen `(lambda (,x k) ,(cps body)))]))])
     (cps1 expr id-gen)))
 
 (cps 'x)
 (cps '(f x))
 (cps '(x (y z)))
+(cps '((m n) (o p)))
 (cps '(lambda (x) (m x)))
 (cps '((lambda (x) (m x)) (lambda (q) (lambda (a) b))))
-'((lambda (x k) (m x k)) (lambda (q k) (k (lambda (a k) (k b)))) k)
+
 
 
 (define (cps-without-eta-optimize expr)  
@@ -42,13 +46,13 @@
            [cps1 (lambda (expr codegen)
                    (match expr
                      [atom #:when (atom? atom) (codegen expr)]
-                     [`(,app ,rator) #:when (and (atom? app) (atom? rator))
+                     [`(,app ,rand) #:when (and (atom? app) (atom? rand))
                                      (let ([x (gensym "var")])
-                                       `(,app ,rator (lambda (,x) ,(codegen x))))]
-                     [`(,app ,rator) (cps1 rator (lambda (rpiece)
+                                       `(,app ,rand (lambda (,x) ,(codegen x))))]
+                     [`(,app ,rand) (cps1 rand (lambda (rpiece)
                                                    (cps1 app (lambda (apiece)
                                                                (cps1 `(,apiece ,rpiece) codegen)))))]
                      [`(lambda (,x) ,body) (codegen `(lambda (,x k) ,(cps1 body id-gen)))]))])
     (cps1 expr id-gen)))
 
-(cps-without-eta-optimize '((lambda (x) (m x)) (lambda (q) (lambda (a) b))))
+(cps-without-eta-optimize '(m n))
